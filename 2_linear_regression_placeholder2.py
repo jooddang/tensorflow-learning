3734239db3add6a4a9848127b3de4d79741ca828
eunkwang <eunkwang@nextdoor.com>
2018-01-01 14:37:37 -0800

    lab 3

---
+++ 2_linear_regression_placeholder2.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import tensorflow as tf
+
+X = tf.placeholder(tf.float32, shape=[None])
+Y = tf.placeholder(tf.float32, shape=[None])
+W = tf.Variable(tf.random_normal([1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+hypothesis = W * X + b
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+train = optimizer.minimize(cost)
+s = tf.Session()
+s.run(tf.global_variables_initializer())
+for step in range(2001):
+    s.run(train, feed_dict={X: [1,2,3], Y: [1,2,3]})
+    if step % 100 == 0:
+        print(step, s.run(W), s.run(b))
+        
